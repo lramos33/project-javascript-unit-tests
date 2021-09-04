bc5b50c65e625ce91e6c607c9c9688642e49b671
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-09-04 17:13:16 -0300

    adequa lint

--- src/vqv.js
+++ src/vqv.js
@@ -18,11 +18,11 @@
 
 const vqv = (nome, idade) => {
   if (nome === undefined && idade === undefined) {
-    return undefined
-  } return (  `Oi, meu nome é ${nome}!
+    return undefined;
+  } return (`Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`)
+#VQV!`);
 };
 
 module.exports = vqv;
