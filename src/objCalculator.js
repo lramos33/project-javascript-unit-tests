a761f18fbf3283dacc2c6629f887d1b0c4acf040
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-09-05 14:42:17 -0300

    adequa lint

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -23,9 +23,7 @@ const calculator = {
     add: (number1, number2) => Math.floor(number1 + number2),
     mult: (number1, number2) => Math.floor(number1 * number2),
     div: (number1, number2) => Math.floor(number1 / number2),
-    sub: (number1, number2) => Math.floor(number1 - number2)
+    sub: (number1, number2) => Math.floor(number1 - number2),
 };
-
-console.log(calculator.div(5, 2))
 
 module.exports = calculator;
